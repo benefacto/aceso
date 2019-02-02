f7da85cb75123f303006e83ca132747b3b4f9729
Ben Walker <ben.walker18@gmail.com>
2019-02-02 15:33:55 -0800

    Modified startup script to permit file sharing

--- scripts/startupNode.sh
+++ scripts/startupNode.sh
@@ -1,1 +1,1 @@
-geth --datadir datadir/ --networkid 458309872615249 --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal"
+geth --datadir datadir/ --networkid 458309872615249 --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --ws --wsorigins="file://" --wsapi="eth,web3,personal" console
